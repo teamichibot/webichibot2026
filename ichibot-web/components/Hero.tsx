91f6febd6f88a442baaf06f3e5168013f8dbd5e8
Team Ichibot <team.ichibo@gmail.com>
2025-11-27 12:33:09 +0700

    perbaikan icon

---
+++ ichibot-web/components/Hero.tsx
@@ -0,0 +1,10 @@
+// components/Hero.tsx
+import React from 'react';
+
+export default function Hero() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-50">
+      <h2 className="text-3xl font-bold">Ini adalah Placeholder Hero Section</h2>
+    </div>
+  );
+}
